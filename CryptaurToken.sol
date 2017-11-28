3fb67cc788e424846bf468aae4b8c75b349fd5db
ILYA SVIRIN <i.svirin@nordavind.ru>
2017-11-28 16:44:29 +0300

    ERC20 operations available in any time

--- CryptaurToken.sol
+++ CryptaurToken.sol
@@ -65,11 +65,6 @@ contract CryptaurToken is owned {
         _;
     }
 
-    modifier enabled {
-        require(crowdsaleFinished == true);
-        _;
-    }
-
     function CryptaurToken(address _cryptaurBackend) public payable owned() {
         cryptaurBackend = _cryptaurBackend;
     }
@@ -92,7 +87,7 @@ contract CryptaurToken is owned {
     }
 
     function transfer(address _to, uint256 _value)
-        public enabled onlyPayloadSize(2 * 32) {
+        public onlyPayloadSize(2 * 32) {
         require(balanceOf[msg.sender] >= _value);
         require(balanceOf[_to] + _value >= balanceOf[_to]);
         balanceOf[msg.sender] -= _value;
@@ -101,7 +96,7 @@ contract CryptaurToken is owned {
     }
     
     function transferFrom(address _from, address _to, uint _value)
-        public enabled onlyPayloadSize(3 * 32) {
+        public onlyPayloadSize(3 * 32) {
         require(balanceOf[_from] >= _value);
         require(balanceOf[_to] + _value >= balanceOf[_to]); // overflow
         require(allowed[_from][msg.sender] >= _value);
@@ -111,7 +106,7 @@ contract CryptaurToken is owned {
         Transfer(_from, _to, _value);
     }
 
-    function approve(address _spender, uint _value) public enabled {
+    function approve(address _spender, uint _value) public {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
     }
